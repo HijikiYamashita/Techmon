29e6ba8b07e9d4ee5c91013bf2643c0673477bf1
HijikiYamashita <godenter792@gmail.com>
2020-05-24 16:23:25 +0900

    チュパカブラ

---
+++ Assets/Scripts/CountScripts.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CountScripts : MonoBehaviour
+{
+    public Text text;
+    public int number;
+    public Text damage;
+    public int total;
+    public AudioSource sound;
+
+    void Start()
+    {
+        number = 0;
+        sound = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (total >= 100)
+        {
+            SceneManager.LoadScene("Clear");
+        }
+    }
+
+    public void CountUp()
+    {
+        number = number + 1;
+        text.text = number.ToString();
+    }
+
+    public void Attack()
+    {
+        total = total + number;
+        damage.text = total.ToString();
+        sound.PlayOneShot(sound.clip);
+    }
+}
---
+++ Assets/Scripts/SoundScripts.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundScripts : MonoBehaviour
+{
+    private static bool isCreated = false;
+
+    void Start()
+    {
+        if (isCreated)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+            isCreated = true;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
